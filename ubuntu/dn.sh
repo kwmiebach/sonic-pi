4016e2961f087255c6b26359aed84d545277babe
kwmiebach <kwmiebach@localhost>
2023-07-23 14:43:08 +0200

    docker compose, Dockerfile and up and down scripts

--- ubuntu/dn.sh
+++ ubuntu/dn.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 
-mkdir -p $HOME/.sonic-pi
+#!/bin/bash
 
 # Check which docker compose command to use
 DOCKER_COMPOSE=$(command -v docker-compose &>/dev/null && echo "docker-compose" || echo "docker compose")
 
-# Run the docker compose file, add -d to run in the background
-$DOCKER_COMPOSE up
+# Run the docker compose file
+$DOCKER_COMPOSE down
--- ubuntu/it.sh
+++ ubuntu/it.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-mkdir -p $HOME/.sonic-pi
-
 # Check which docker compose command to use
 DOCKER_COMPOSE=$(command -v docker-compose &>/dev/null && echo "docker-compose" || echo "docker compose")
 
-# Run the docker compose file, add -d to run in the background
-$DOCKER_COMPOSE up
+# Run bash in the container
+$DOCKER_COMPOSE exec -u 1000 -it sonic-pi /bin/bash
